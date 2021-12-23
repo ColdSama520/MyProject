18a4fd2c33c08debcde626932375c1a8a2022b34
ColdSama520 <1360614758@qq.com>
2021-12-23 11:59:35 +0800

    cold

--- src/main/java/cn/edu/bjfu/javawebproject/JavaWebProjectApplication.java
+++ src/main/java/cn/edu/bjfu/javawebproject/JavaWebProjectApplication.java
@@ -3,6 +3,10 @@ package cn.edu.bjfu.javawebproject;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @MapperScan("cn.edu.bjfu.javawebproject.mapper")
 @SpringBootApplication
@@ -11,5 +15,21 @@ public class JavaWebProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaWebProjectApplication.class, args);
     }
+
+    //全局配置跨域
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //针对某一网址 且请求路径是两级/../...
+                //registry.addMapping("/*/*").allowedOrigins("http://10.252.96.120");
+                //针对所有网址，且请求路径是多级/  or /../..  or /../../...
+                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**").allowCredentials(false);
+            }
+        };
+    }
+
 
 }
--- src/main/java/cn/edu/bjfu/javawebproject/controller/UserController.java
+++ src/main/java/cn/edu/bjfu/javawebproject/controller/UserController.java
@@ -3,19 +3,23 @@ package cn.edu.bjfu.javawebproject.controller;
 import cn.edu.bjfu.javawebproject.pojo.User;
 import cn.edu.bjfu.javawebproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/User")
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @PostMapping("/login")
+    public int Login(@RequestBody User user){
+        System.out.println(user.getUser_id());
+        System.out.println(user.getUser_pwd());
+        return 200;
+    }
 
     @GetMapping("/{user_id}")
     public User GetUser(@PathVariable String user_id){
