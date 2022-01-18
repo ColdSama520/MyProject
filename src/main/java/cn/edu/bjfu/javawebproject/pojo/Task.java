e055799d65e1be46dfbe06ab9fc3c294590951db
ColdSama520 <1360614758@qq.com>
2022-01-18 19:07:46 +0800

    学生第2项over

--- src/main/java/cn/edu/bjfu/javawebproject/pojo/Task.java
+++ src/main/java/cn/edu/bjfu/javawebproject/pojo/Task.java
@@ -10,15 +10,15 @@ import lombok.NoArgsConstructor;
 public class Task {
     private String task_id;
     private String task_name;
-    private String task_details;
-    private String task_stage_type;
+    private String task_details; // 任务细节
+    private String task_stage_type;  // 所属任务阶段
     private String student_id;
-    private String task_start_date;
-    private String task_alter_date;
-    private String task_score;
-    private String task_reviews;
-    private String task_score_date;
-    private String task_type;
-    private String task_start_day;
-    private String task_start_time;
+    private String task_start_date;  //  任务生成时间
+    private String task_alter_date;   // 任务修改时间
+    private String task_score;   // 任务评分
+    private String task_reviews;   // 任务点评
+    private String task_score_date;   // 任务评分时间
+    private String task_type;   // 任务状态
+    private String task_start_day;   //  任务拟开始时间
+    private String task_start_time;   // 任务拟工作时长
 }
