5e08cc7b82f2dfba90d48e8c5151ec36a027ffa8
ColdSama520 <1360614758@qq.com>
2022-01-25 13:28:08 +0800

    中期完成

--- src/main/java/cn/edu/bjfu/javawebproject/controller/UploadController.java
+++ src/main/java/cn/edu/bjfu/javawebproject/controller/UploadController.java
@@ -9,11 +9,9 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.xml.transform.Result;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -35,6 +33,29 @@ public class UploadController {
 
     }
 
+    @GetMapping("/download")
+    public void logDownload(String name, HttpServletResponse response) throws Exception {
+//        File file = new File("C:/Users/13606/Desktop/UpLoad-cs/" + File.separator + name);
+        File file = new File(File.separator + name);
+        if (!file.exists()) {
+            System.out.println(name + "文件不存在");
+        }
+        response.setContentType("application/force-download");
+        response.addHeader("Content-Disposition", "attachment;fileName=" + name);
+
+        byte[] buffer = new byte[1024];
+        try (FileInputStream fis = new FileInputStream(file);
+             BufferedInputStream bis = new BufferedInputStream(fis)) {
+
+            OutputStream os = response.getOutputStream();
+
+            int i = bis.read(buffer);
+            while (i != -1) {
+                os.write(buffer, 0, i);
+                i = bis.read(buffer);
+            }
+        }
+    }
 
 
 }
